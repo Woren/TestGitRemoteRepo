3d2b6ada36a7f05ac27db8a4362a89ee294efa20
Michal Havryluk <michal.havryluk@gmail.com>
2013-10-03 15:38:31 +0200

    Treti uprava + mobile branch

--- src/cz/gittest/android/MainActivity.java
+++ src/cz/gittest/android/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("Druha uprava + mobile branch");
+		System.out.println("Treti uprava + mobile branch");
 	}
 
 	@Override
