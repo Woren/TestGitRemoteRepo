fb48ddc379dce97a93ef1776b69a1233811c6970
Michal Havryluk <michal.havryluk@gmail.com>
2013-10-03 15:35:29 +0200

    Prvni uprava + mobile branch

--- src/cz/gittest/android/MainActivity.java
+++ src/cz/gittest/android/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("Prvni uprava");
+		System.out.println("Prvni uprava + mobile branch");
 	}
 
 	@Override
